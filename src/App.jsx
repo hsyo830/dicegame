a5affd47d0b08a537dddc1f2fa33280928c43269
황서영 <nct830@naver.com>
2025-10-15 11:53:07 +0900

    chore: 기본 컴포넌트 2개 생성 및 이미지 파일 추가 (기능 미구현)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dice from "./Dice";
+
+function App() {
+  // 주사위 게임 로직 구현
+  return (
+    <div>
+      <h1>주사위 게임</h1>
+      <Dice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dice.jsx
@@ -0,0 +1,7 @@
+import dicePurple01 from "./assets/dice-purple-1.svg";
+
+function Dice() {
+  return <img src={dicePurple01} alt="주사위" />;
+}
+
+export default Dice;
