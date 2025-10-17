4fb9a515ab02e353106dc364faf7011cfe51f76f
황서영 <nct830@naver.com>
2025-10-17 17:32:32 +0900

    feat: 게임 총점과 게임플레이 기록 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,19 @@ function random(n) {
 function App() {
   // 주사위 게임 로직 구현
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -25,7 +31,12 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="red" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="red" num={num} />
+        <div>총점: {sum}</div>
+        <div>기록: {gameHistory.join(", ")}</div>
+      </div>
     </div>
   );
 }
